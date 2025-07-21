14909e20f74c9144997da1c9241ee13cf5ab068a
Farid <fchipanam@unjbg.edu.pe>
2025-07-20 21:24:13 -0500

    Agregue structura animal, veterinario y dieta

--- proyecto.cpp
+++ proyecto.cpp
@@ -4,7 +4,42 @@ using namespace std;
 
 const int MAX = 100;
 
+struct Veterinario {
+    string nombre;
+    int anios_exp;
+};
+
+struct Dieta {
+    string tipo_alimento;
+    int cantidad_diaria;
+    string frecuencia;
+};
+
+struct Animal {
+    string nombre;
+    string especie;
+    char sexo;
+    int edad;
+    string habitat;
+    Veterinario encargado;
+    Dieta alimentacion;
+};
+
+void agregarAnimal(Animal animales[], int &n) {
+    if (n >= MAX) return;
+
+    cout << "Nombre del animal: ";
+    getline(cin, animales[n].nombre);
+    cout << "Especie: ";
+    getline(cin, animales[n].especie);
+    cout << "Sexo (M/H): ";
+    cin >> animales[n].sexo;
+    n++;
+}
+
+
 int main() {
     // Aui implementaremos todo
     return 0;
 }
+
