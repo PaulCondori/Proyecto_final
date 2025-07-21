991b77fdf2a4bc450c1f1891a1d090a9d7c68c58
PaulCondori <pscondoric@unjbg.edu.pe>
2025-07-20 21:34:15 -0500

    Revert "Corregi agregaranimal añadiendo el cin.ignore()"

--- proyecto.cpp
+++ proyecto.cpp
@@ -27,7 +27,6 @@ struct Animal {
 
 void agregarAnimal(Animal animales[], int &n) {
     if (n >= MAX) return;
-    cin.ignore();
 
     cout << "Nombre del animal: ";
     getline(cin, animales[n].nombre);
@@ -35,13 +34,6 @@ void agregarAnimal(Animal animales[], int &n) {
     getline(cin, animales[n].especie);
     cout << "Sexo (M/H): ";
     cin >> animales[n].sexo;
-    cin.ignore();
-    cout << "Edad: ";
-    cin >> animales[n].edad;
-    cin.ignore();
-    cout << "Habitat: ";
-    getline(cin, animales[n].habitat);
-    // Agregar el resto lo de veterinario y dieta eh
     n++;
 }
 
